30ed33429a482f63384b968a5007e42aa6bd312d
aTenshouSu <zou.tianxiang@access-company.com>
2019-04-18 11:05:46 +0900

    Add the hello world function

--- app/src/main/java/io/access/kotlintraining/Sample.kt
+++ app/src/main/java/io/access/kotlintraining/Sample.kt
@@ -4,4 +4,12 @@ fun welcome() {
     println("welcome to ACCESS!")
 }
 
+fun hello() {
+    println("Hello World!")
+}
+
+fun main(args: Array<String>) {
+    welcome()
+    hello()
+}
 // TODO: declare hello function
